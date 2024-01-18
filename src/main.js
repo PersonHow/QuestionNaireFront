7e34aa8f2bda5bf344e884751a8e0dbd436b6051
AppleHowMuch <SecreTary_RC@hotmail.com>
2024-01-18 18:07:39 +0800

    GOGO

--- src/main.js
+++ src/main.js
@@ -1,9 +1,5 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-<<<<<<< HEAD
-=======
-
->>>>>>> 50feaf39c9ce5cc9d1ffe96ea9cb6ddf6e128c77
 import App from './App.vue'
 import router from './router'
 
@@ -13,8 +9,4 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-<<<<<<< HEAD
-=======
-
->>>>>>> 50feaf39c9ce5cc9d1ffe96ea9cb6ddf6e128c77
 app.mount('#app')
